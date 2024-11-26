d8b11e32311b24a4ca258ed60c87d9039114704d
Swapna <swapna.a.p@capgemini.com>
2024-11-26 20:06:35 +0530

    SetUp Project

---
+++ app/src/main/java/com/swapna/carlist/car/CarActivity.kt
@@ -0,0 +1,13 @@
+package com.swapna.carlist.car
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.swapna.carlist.R
+
+class CarActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.car_activity)
+    }
+}

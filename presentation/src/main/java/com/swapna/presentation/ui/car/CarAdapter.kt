89e792009919d0eecb1894a2c19b45f1be36cd5b
Swapna <swapna.a.p@capgemini.com>
2024-11-27 17:12:32 +0530

    Implement custom tab

--- presentation/src/main/java/com/swapna/presentation/ui/car/CarAdapter.kt
+++ presentation/src/main/java/com/swapna/presentation/ui/car/CarAdapter.kt
@@ -1,7 +1,9 @@
 package com.swapna.carlist.ui.car
 
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.swapna.domain.model.Car
@@ -15,6 +17,13 @@ class CarAdapter(val carList:ArrayList<Car>) : RecyclerView.Adapter<CarAdapter.C
             binding.txtPrice.text = car.price
             binding.txtMileage.text = car.mileage
             Glide.with(binding.imgBanner.context).load(car.image).into(binding.imgBanner)
+
+            itemView.setOnClickListener {
+                val builder = CustomTabsIntent.Builder()
+                val customTabsIntent = builder.build()
+                customTabsIntent.launchUrl(it.context, Uri.parse(car.image))
+            }
+
         }
     }
 
